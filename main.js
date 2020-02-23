3f45322464cd022562598e60d4cf5b5d97dff36a
Josue Iglesias <jiglesias3@ucol.mx>
2020-02-22 23:56:14 -0600

    clase producto

--- main.js
+++ main.js
@@ -1,4 +1,5 @@
 import Precio from "./precio.js"
+import Producto from "./producto.js"
 
 class Main {
     constructor(){
@@ -10,7 +11,14 @@ class Main {
         let precio1 = new Precio(443)
         console.log(precio1.getPrecio())
     }
+
+    probarProducto(){
+        console.log("---------Producto----------")
+        let producto1 = new Producto("Tortaogada estilo colimote", new Precio(49))
+        console.log(producto1.getDescripcion())
+    }
 }
 
 let app = new Main
 app.probarPrecio()
+app.probarProducto()
